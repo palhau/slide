62aa726ff5e63e2b2351f6ef9b07065ff96afcc2
Alan Palhau <alanvieirapalhau@hotmail.com>
2020-01-08 14:25:49 -0300

    Ajuste das configuracoes ao resize e classe ativa adicionada

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-import Slide from './slide.js'
+import Slide from './slide.js';
 
 const slide = new Slide('.slide', '.slide-wrapper');
 slide.init();
+
+slide.changeSlide(3);
+slide.activePrevSlide();
